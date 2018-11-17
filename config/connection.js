c566a39631fe3b6d6f7e21d069651e1f8da6c0d2
Diloretodg <diloretodg74122@gmail.com>
2018-11-17 10:40:48 -0500

    jaws db

--- config/connection.js
+++ config/connection.js
@@ -1,14 +1,23 @@
+
 var mysql = require("mysql");
 
-var connection = mysql.createConnection({
+var connection 
+
+//calling heroku enviromental var 
+if(process.env.JAWSDB_URL){
+connection = mysql.createConnection(process.env.JAWSDB_URL);
+}else{
+  connection = mysql.createConnection({
   host: "localhost",
   port: 3306,
   user: "root",
   password: "Admin!",
-  database: "burgers_db"
+  database: "EDB_db"
+
 });
+}
 
-
+//connect EDB_DB
 connection.connect(function(err) {
   if (err) {
     console.error("error connecting: " + err.stack);
@@ -17,4 +26,5 @@ connection.connect(function(err) {
   console.log("connected as id " + connection.threadId);
 });
 
+//export for ORM 
 module.exports = connection;
